7d7dd69e92dcb5358d0d5097e9b896bfaf2a3c4b
rajashekaran26 <rajashekaran26@gmail.com>
2019-12-01 12:57:11 +0530

    Updates

---
+++ precache-manifest.53853ab391810f383e50eaf0073ba956.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c054ed170d07afe37680fd251de2218",
+    "url": "/cuddly-spork/index.html"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/cuddly-spork/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f3d70acbc3b8df62bc8e",
+    "url": "/cuddly-spork/static/css/main.d9b03cf9.chunk.css"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/cuddly-spork/static/js/2.49b130ef.chunk.js"
+  },
+  {
+    "revision": "f3d70acbc3b8df62bc8e",
+    "url": "/cuddly-spork/static/js/main.80f25619.chunk.js"
+  },
+  {
+    "revision": "d503f973f2f42ffd2ccb",
+    "url": "/cuddly-spork/static/js/runtime-main.40ec8283.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/cuddly-spork/static/media/SEGA.12a3db8b.woff"
+  }
+]);
